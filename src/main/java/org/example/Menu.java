fe2e295812875ab4f4f62989644644c22e1078d5
Stanly <stanlyalmeidabr@gmail.com>
2025-06-16 17:23:07 -0300

    add controller e service

--- src/main/java/org/example/Menu.java
+++ src/main/java/org/example/Menu.java
@@ -7,7 +7,7 @@ import org.example.model.UsuarioModel;
 
 import java.util.Scanner;
 
-public class Menu {
+public class enu {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Biblioteca biblioteca = new Biblioteca();
---
+++ src/main/java/org/example/rost/controller/UsuarioControlle.java
@@ -0,0 +1,4 @@
+package org.example.rost.controller;
+
+public class UsuarioControlle {
+}
